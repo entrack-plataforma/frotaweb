7eb3b3258e119cc4bc2dd81920473ef64e7abe35
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2026-01-28 17:31:31 +0000

    log requests performance

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,8 +1,11 @@
-export const onRequest = ({request, env}, cf) => {
+export const onRequest = async ({request, env}, cf) => {
+    const now = performance.now()
     const url = new URL(request.url)
     url.host = env.TRACCAR_SERVER || 'gps.frotaweb.com'
     url.protocol = 'http:'
     url.port = 80
     console.log(performance.now())
-    return fetch(new Request(url, request), cf)
+    const response = await fetch(new Request(url, request), cf)
+    console.log(performance.now() - now, 'ms')
+    return response
 }
